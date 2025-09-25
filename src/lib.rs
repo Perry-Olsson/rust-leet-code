95282fce891cd8cc22d748e80ee9ec836330dce7
Perry Olsson <olsson.perry@gmail.com>
2025-09-24 18:51:17 -0700

    Simple remove element

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 mod remove_duplicates_from_sorted_array;
 mod index_of_first_occurence_in_string;
+mod remove_element;
 
 pub fn run() {
     let mut input: Vec<i32> = vec![1, 2, 2, 3];
     remove_duplicates_from_sorted_array::Solution::remove_duplicates(&mut input);
     index_of_first_occurence_in_string::Solution::str_str(String::from("leetcode"), String::from("leeto"));
+    remove_element::Solution::remove_element(&mut vec![3, 2, 2, 3], 3);
 }
---
+++ src/remove_element.rs
@@ -0,0 +1,103 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+        nums.retain(|num| *num != val);
+        fn hello() {
+        }
+        nums.len() as i32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let mut actual = vec![3, 2, 2, 3];
+        let expected = vec![2, 2];
+        let new_array_len = Solution::remove_element(&mut actual, 3);
+        assert_eq!(2, new_array_len);
+        assert_equality(expected, actual);
+    }
+
+    #[test]
+    fn test_2() {
+        let mut actual = vec![0, 1, 2, 2, 3, 0, 4, 2];
+        let expected = vec![0, 1, 4, 0, 3];
+        let new_array_len = Solution::remove_element(&mut actual, 2);
+        assert_eq!(5, new_array_len);
+        assert_equality(expected, actual);
+    }
+
+    #[test]
+    fn test_3() {
+        let mut actual = vec![];
+        let expected = vec![];
+        let new_array_len = Solution::remove_element(&mut actual, 3);
+        assert_eq!(0, new_array_len);
+        assert_equality(expected, actual);
+    }
+
+    #[test]
+    fn test_4() {
+        let mut actual = vec![4];
+        let expected = vec![4];
+        let new_array_len = Solution::remove_element(&mut actual, 2);
+        assert_eq!(1, new_array_len);
+        assert_equality(expected, actual);
+    }
+
+    #[test]
+    fn test_5() {
+        let mut actual = vec![1, 1, 1];
+        let expected = vec![];
+        let new_array_len = Solution::remove_element(&mut actual, 1);
+        assert_eq!(0, new_array_len);
+        assert_equality(expected, actual);
+    }
+
+    #[test]
+    fn test_6() {
+        let mut actual = vec![1, 1, 1, 3, 1, 1, 1];
+        let expected = vec![3];
+        let new_array_len = Solution::remove_element(&mut actual, 1);
+        assert_eq!(1, new_array_len);
+        assert_equality(expected, actual);
+    }
+
+    #[test]
+    fn test_7() {
+        let mut actual = vec![5, 3, 11];
+        let expected = vec![5, 3, 11];
+        let new_array_len = Solution::remove_element(&mut actual, 56);
+        assert_eq!(3, new_array_len);
+        assert_equality(expected, actual);
+    }
+
+    #[test]
+    fn test_8() {
+        let mut actual = vec![4];
+        let expected = vec![];
+        let new_array_len = Solution::remove_element(&mut actual, 4);
+        assert_eq!(0, new_array_len);
+        assert_equality(expected, actual);
+    }
+
+    fn assert_equality(mut expected: Vec<i32>, mut actual: Vec<i32>) -> bool {
+        expected.sort();
+        actual.sort();
+        let mut idx = 0;
+        while idx < expected.len() {
+            if expected[idx] != actual[idx] {
+                println!("Test Failed!");
+                println!("expected: {:?}", expected);
+                println!("actual: {:?}", actual);
+                return false;
+            }
+            idx += 1;
+        }
+        return true;
+    }
+}
