9c5705e79e4459e18b5f961a35c64ffb501fdc29
Perry Olsson <olsson.perry@gmail.com>
2025-09-24 18:51:42 -0700

    remove

--- src/remove_element.rs
+++ src/remove_element.rs
@@ -3,8 +3,6 @@ pub struct Solution {}
 impl Solution {
     pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
         nums.retain(|num| *num != val);
-        fn hello() {
-        }
         nums.len() as i32
     }
 }
